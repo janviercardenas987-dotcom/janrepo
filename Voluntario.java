80df5c80a4f0c32894b7562febff3f96c14cc993
janvier <janviercardenas987@gmail.com>
2026-07-09 18:19:58 -0400

    git en la rama testing

---
+++ Voluntario.java
@@ -0,0 +1,40 @@
+package Product;
+
+public class Voluntario extends Persona{
+    private int horasTrabajadas;
+    private String profesion;
+
+    public Voluntario(String dni, String nombre, String apellido, String telefono, int horasTrabajadas, String profesion) {
+        super(dni, nombre, apellido, telefono);
+        this.horasTrabajadas = horasTrabajadas;
+        this.profesion = profesion;
+    }
+
+    public int getHorasTrabajadas() {
+        return horasTrabajadas;
+    }
+
+    public void setHorasTrabajadas(int horasTrabajadas) {
+        this.horasTrabajadas = horasTrabajadas;
+    }
+
+    public String getProfesion() {
+        return profesion;
+    }
+
+    public void setProfesion(String profesion) {
+        this.profesion = profesion;
+    }
+
+    @Override
+    public String toString() {
+        return "Voluntario{" +
+                "horasTrabajadas=" + horasTrabajadas +
+                ", profesion='" + profesion + '\'' +
+                ", dni='" + dni + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", telefono='" + telefono + '\'' +
+                '}';
+    }
+}
